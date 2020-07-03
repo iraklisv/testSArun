529c9e97536d28ec8238a59320e38a757002cdc6
iraklisv <shadowr5@gmail.com>
2020-07-03 10:01:50 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VMS.TPS.Common.Model.API;
+
+namespace testSArun
+{
+    class Program
+    {
+
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            try
+            {
+                using (Application app = Application.CreateApplication())
+                {
+                    Perform(app);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e.ToString());
+                Console.Read();
+                //  MessageBox.Show(e.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        public static void Perform(Application app)
+        {
+            Console.WriteLine($"loggin {app.CurrentUser.ToString()}");
+            Console.ReadLine();
+        }
+    }
+}
